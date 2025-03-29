f6338788595fc93adfea83a7e6e58777b112dce9
madarasz.oli <madaraszoliver@gmail.com>
2025-03-29 16:09:55 +0100

    base html added

---
+++ HotelRegistration/hotel.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Register</h1>
+        <form action="" method="POST">
+            <h1>Whats your name?<h1>
+                <input type="text" name="fullname" value="fullname"></input>
+            <h1>Whats your ID?<h1>
+                <input type="text" name="id" value="id"></input>
+            <h1>When will you arrive?<h1>
+                <input type="date" name="arrivedate" value="arrivedate"></input>
+            <h1>When will you depart?<h1>
+                <input type="date" name="depdate" value="depdate"></input>
+        </form>
+    </body>
+
+</html>
