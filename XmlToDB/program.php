eab970060798992947e2ccfb9f3b0727b124bf97
madarasz.oli <madaraszoliver@gmail.com>
2025-04-03 13:56:08 +0200

    DB connected

--- XmlToDB/program.php
+++ XmlToDB/program.php
@@ -5,6 +5,32 @@
 //3. create a DB for the xml values
 //4. insert the values into the DB
 
+include("login.php");
+
+$conn= new mysqli($host,$user,$password);
+
+if($conn->connect_error)
+{
+    die("connection falied: ".$conn->connect_error."<br>");
+}
+else
+{
+    echo "Connection success"."<br>";
+}
+
+$sql_create_db= "CREATE DATABASE IF NOT EXISTS zoo";
+if($conn->query($sql_create_db))
+{
+    echo "database created succesfully"."<br>";
+}
+else
+{
+    die ("error creating database: ". $conn->error."<br>");
+}
+
+$conn->select_db("zoo");
+//dynamically creating DB from the xml
+
 //1.
 //xml loading
 
@@ -24,7 +50,7 @@ else
 
         foreach($array as $subChild)
         {
-            //insert into DB
+            //insert into DB here
             echo $subChild->getName(). ":" . $subChild."<br>";
         }
 
